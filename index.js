179b1b09281fc018b33deef6bc9c877c111c5ff9
Anton Mihail Lachmaniucu <anton2mihail@users.noreply.github.com>
2018-11-20 12:24:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,11 @@ function newTask(title, description) {
   return task;
 }
 
-// DRIVER CODE CHANGES BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-task1.logState(); // Clean Cat Litter has not been completed
+task1.logState();
 task1.markCompleted();
-task1.logState(); // Clean Cat Litter has been completed
+task1.logState();
